c68b60082a8f32142bc5834c5441e8e9328abdf0
UBauddhik <125404632+UBauddhik@users.noreply.github.com>
2023-10-03 13:01:50 -0400

    card copied

--- src/last-try.js
+++ src/last-try.js
@@ -1,88 +1,33 @@
-import { LitElement, html, css } from 'lit';
+import { html, css, LitElement } from 'lit';
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
-
-class LastTry extends LitElement {
-  static properties = {
-    header: { type: String },
-  }
-
+export class MyCard extends LitElement {
   static styles = css`
     :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--last-try-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
+      display: block;
+      padding: 25px;
+      color: var(--my-card-text-color, #000);
     }
   `;
 
+  static properties = {
+    header: { type: String },
+    counter: { type: Number },
+  };
+
   constructor() {
     super();
-    this.header = 'My app';
+    this.header = 'Hey there';
+    this.counter = 5;
+  }
+
+  __increment() {
+    this.counter += 1;
   }
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/LastTry.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <h2>${this.header} Nr. ${this.counter}!</h2>
+      <button @click=${this.__increment}>increment</button>
     `;
   }
 }
-
-customElements.define('last-try', LastTry);
