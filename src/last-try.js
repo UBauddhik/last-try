6dfb322ed8d906c59f5bf5574717fdc8d90d62ae
UBauddhik <125404632+UBauddhik@users.noreply.github.com>
2023-10-16 17:11:25 -0400

    Card working, to be further modified

--- src/last-try.js
+++ src/last-try.js
@@ -1,89 +1,187 @@
-import { html, css, LitElement } from 'lit';
+import { LitElement, html, css } from 'lit';
 
-export class LastTry extends LitElement {
+
+class LastTry extends LitElement {
+  static properties = {
+    image: { type: String },
+    alt: { type: String },
+    title: { type: String },
+    description: { type: String },
+    dark: { type: Boolean, reflect: true }
+  }
+
   static styles = css`
     :host {
-      display: block;
-      padding: 25px;
-      color: var(--my-card-text-color, #000);
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      font-size: calc(10px + 2vmin);
+      color: black;
+      max-width: 960px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: var(--last-try-background-color);
     }
-  `;
 
-__changeTitle(){
-  if(this.title=="Description"){
-    this.title="Something Else"
+    body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    background-color: white;
   }
-  else{
-    this.title="Description"
+
+  .wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .container {
+    max-width: 400px;
+    border: solid black;
+    border-radius: 8px;
+    margin: 16px;
+    padding: 16px;
+    background-color: white;
+  }
+
+  .container img {
+    width: 100%;
+    height: auto;
+    border-radius: 10px;
+  }
+
+  .card-content {
+    margin-top: 0;
+    text-align: center;
+  }
+
+  .card-title {
+    margin: 16px;
+    text-align: center;
+  }
+
+  .description {
+    margin: 4px;
+  }
+
+  .toggle-button {
+    display: block;
+    text-align: center;
+    padding: 8px;
+    border: solid black;
+    border-radius: 4px;
+    margin-top: 16px;
+    color: black;
+    background-color: #0074D9;
+  }
+  .details-link {
+    display: none;
+    margin-top: 16px;
+    text-align: center;
+  }
+
+  .toggle-button:hover,
+  .toggle-button:focus {
+    background-color: #0056b3;
+  }
+
+  .light-mode {
+    background-color: #ffffff;
+    color: #000000;
+  }
+  .dark-mode {
+    background-color: #000000;
+    color: #ffffff;
+  }
+
+  .hidden {
+    display: none;
+  }
+  @media (max-width: 800px) {
+    .button-details {
+      display: none;
+    }
+}`
+
+constructor() {
+  super();
+  this.image = 'https://www.dalycollege.org/images/slider-1.jpg';
+  this.title = 'Card';
+  this.description = 'The Daly College is a group of institutions in Indore, India that includes a co-educational boarding school, a junior school, an undergraduate management school, and a postgraduate business school.';
+  }
+
+duplicateCard() {
+  const cardTemplate = this.shadowRoot.querySelector('.container');
+  const clone = cardTemplate.cloneNode(true);
+
+  clone.querySelector('.toggle-button').classList.remove('hidden');
+  const cardDescription = clone.querySelector('.description');
+  const toggleDescriptionButton = clone.querySelector('.toggle-button');
+  
+  toggleDescriptionButton.addEventListener('click', () => {
+    cardDescription.classList.toggle('hidden');
+  });
+
+  this.shadowRoot.querySelector('.wrapper').appendChild(clone);
+}
+
+
+toggleColor() {
+  const card = this.shadowRoot.querySelector('.container');
+  card.classList.toggle('light-mode');
+  card.classList.toggle('dark-mode');
+
+}
+
+toggleDescription() {
+  const cardDescription = this.shadowRoot.querySelector('.description');
+  cardDescription.classList.toggle('hidden');
+}
+
+changeTitle() {
+  const cardTitle = this.shadowRoot.querySelector('.card-title');
+  cardTitle.textContent = 'something else';
+}
+
+deleteLastCard() {
+  const cards = this.shadowRoot.querySelectorAll('.container');
+  if (cards.length > 1) {
+    const lastCard = cards[cards.length - 1];
+    lastCard.remove();
   }
 }
 
-// to select entire card this.parentNode
-// outside card document.querySelector("last-try").shadowRoot.document.querySelector('button').addEventListener('click', (e) => {
-  //   const cardClone = document.querySelector('.card').cloneNode(true);
-  //   document.body.appendChild(cardClone);
-
-  document.querySelector('last-try').shadowRoot.document.queryselector('button').addEventListener('click', (e) => {
-    const cardClone = document.querySelector('.card').cloneNode(true);
-    document.body.appendChild(cardClone);
-  })
-
-  // document.querySelector('#backgroundColor').addEventListener('click', () => {
-  //   const card = document.querySelector('.card');
-  //   card.classList.toggle('light-mode');
-  //   card.classList.toggle('dark-mode');
-  // })
-
-  // document.querySelector('#changeText').addEventListener('click', () => {
-  //   const heading = document.querySelector('h1');
-  //   heading.textContent = "Something Else";
-  // })
-
-  // document.querySelector('#delete').addEventListener('click', () => {
-  //   const cards = document.querySelectorAll('.card');
-  //   if (cards.length > 1) {
-  //       const lastCard = cards[cards.length - 1];
-  //       lastCard.remove();
-  //   }
-  // })
-
-  // document.querySelector('#detailsButton').addEventListener('click', () => {
-  //   const description = document.querySelector('#description');
-  //   description.style.display = (description.style.display === 'none') ? 'block' : 'none';
-  // })
-
-  static properties = {
-    header: { type: String },
-    counter: { type: Number },
-    title: { type: String }
-  };
-
-  constructor() {
-    super();
-    this.header = 'Hey there';
-    this.counter = 5;
-    this.title = "Description";
-  }
-
-
-  render() {
-    return html`
-    <button>Button</button>
-    <div class="all">
-        <button id="backgroundColor">Background Color</button>
-        <button @click=${this.__changeTitle}>Change Text</button>
-        <button id="delete">Delete Last Card</button>
-        <div class="card">
-            <img src="https://www.dalycollege.org/images/slider-1.jpg" >
-            <h1>${this.title}</h1>
-            <p id="description" style="display: none;">
-                The Daly College is a group of institutions in Indore, India that includes a co-educational boarding school, a junior school, an undergraduate management school, and a postgraduate business school.
-            </p>
-            <button id="detailsButton">Details</button>
+render() {
+  return html`
+    <main>
+      <div class="wrapper">
+        <div class="container">
+          <div class="card">
+            <div class="card-content">
+              <img src="${this.image}" >
+              <h2 class="card-title">${this.title}</h2>
+              <div class="description-toggle">
+                <div class="description"><slot>${this.description}</slot></div>
+                <div class="toggle-button" @click="${this.toggleDescription}">Details</div>
+              </div>
+              <a class="details-link" href="${this.link}">Details</a>
+            </div>
+          </div>
         </div>
-    </div>
-    `;
-  }
+      </div>
+      <button class="duplicateButton" @click="${this.duplicateCard}">Duplicate Card</button>
+      <button class="toggleColorButton" @click="${this.toggleColor}">Toggle Color</button>
+      <button class="changeTitleButton" @click="${this.changeTitle}">Change Title</button>
+      <button class="deleteCardButton" @click="${this.deleteLastCard}">Delete Last Card</button>
+    </main>
+  `;
+}
 }
 
-customElements.define('last-try', LastTry)
+customElements.define('last-try', LastTry);
+
